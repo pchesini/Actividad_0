fe2d6f6852156de01ee98e19ae1a41734bf8fb2a
Pablo Chesini <pchesini1@gmail.com>
2024-04-12 17:27:08 -0300

    [Parte4.2] Se crea la vista y templete result para que se puedan ver los resultados despues de votar

--- Tutorial_Django/polls/views.py
+++ Tutorial_Django/polls/views.py
@@ -7,32 +7,17 @@ def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
-#def index(request):
- #   latest_question_list = Question.objects.order_by("-pub_date")[:5]
-  #  template = loader.get_template("polls/index.html")
-   # context = {
-    #    "latest_question_list": latest_question_list,
-    #}
-    #return HttpResponse(template.render(context, request))
-#def index(request):
-    #latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    #output = ", ".join([q.question_text for q in latest_question_list])
-    #return HttpResponse(output)
 
-#def index(request):
-   # return HttpResponse("Hola Mundo. Estás en el índice de encuestas.")
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
-#def detail(request, question_id):
-   # return HttpResponse("Estas viendo la pregunta %s." % question_id)
 
 
 def results(request, question_id):
-    response = "Estás viendo los resultados de la pregunta %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
